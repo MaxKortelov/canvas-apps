528cc4404ae01cf5457c9480fd9c0b39b5b34e55
Max <maksym@mac.local.ua>
2023-05-05 19:01:12 +0200

    fixed naming

--- src/app/pages/games/games.module.ts
+++ src/app/pages/games/games.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { GamesComponent } from './games.component';
 import { PuzzleModule } from './puzzle/puzzle.module';
 import { GamesRoutingModule } from './games-routing.module';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [GamesComponent],
